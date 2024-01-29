e6841f478527cc772ed7991394acebd6b8a04960
stefanosiano <stefano.siano91@gmail.com>
2024-01-29 11:22:22 +0100

    test release workflow

--- deploy_project.sh
+++ deploy_project.sh
@@ -6,6 +6,16 @@ error_exit() {
 
 # Check if gh is installed
 if ! command -v gh &> /dev/null; then
+  type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+fi
+
+# Check if gh is installed
+if ! command -v gh &> /dev/null; then
   error_exit "gh is not installed, make sure you run 'make init' (see README.md)."
 fi
 
